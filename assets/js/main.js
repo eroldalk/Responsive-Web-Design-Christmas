74061665d93643f8079d3f352daa0842c2687ebb
eroldalk <eroldalg@gmail.com>
2022-03-03 23:51:28 +0300

    project finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,14 +58,58 @@ window.addEventListener('scroll', scrollHeader)
 
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top');
-    
+
     if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
 
 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-toggle-right'
 
 
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
 
 
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bx-toggle-right'
 
+if (selectedTheme) {
+ 
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme)
+}
+
+
+themeButton.addEventListener('click', () => {
+    
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+const sr = ScrollReveal({
+    distance: '30px',
+    duration: 1800,
+    reset: true
+});
+
+sr.reveal(`.home__data, .home__img
+        .decoration__data,
+        .accessory__content,
+        .footer__content`, {
+    origin: 'top',
+    interval: 200
+})
+
+sr.reveal(`.share__img, .send__content`,{
+    origin: `left`
+})
+
+sr.reveal(`.share__data, .send__img`,{
+    origin: `right`
+})
